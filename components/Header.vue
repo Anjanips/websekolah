2dc5dcb7878e55a28325b4fe5784499e7bb83048
Anjanips <143574069+Anjanips@users.noreply.github.com>
2024-12-24 14:44:33 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="assets/img/logo smk(1).png" alt="Logo" width="65" height="57" class="d-inline-block align-text-bootom">
+      SMKN 4 TASIKMALAYA
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           PROFIL
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="profil/">SEJARAH</a></li>
+            <li><a class="dropdown-item" href="visi">VISI & MISI</a></li>
+            <li><a class="dropdown-item" href="#">JURUSAN</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">EKSTRAKURIKULER</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">GALERI</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+<style>
+
+.collapse{
+    padding-left: 60% ;
+
+}
+</style>
